f3e334f3c513384728c463e4ae82d39b8f234978
Ubuntu <ubuntu@ip-172-31-37-130.us-west-2.compute.internal>
2021-03-11 00:26:57 +0000

    add code to compute robust stats using many runs

--- train.py
+++ train.py
@@ -2,8 +2,7 @@ from models import *
 from data import *
 
 import numpy as np
-import scipy.sparse as sp
-import argparse, copy
+import argparse
 
 def get_parser():
     parser = argparse.ArgumentParser()
@@ -11,6 +10,7 @@ def get_parser():
     # Experiment Parameters
     parser.add_argument('--model_type', type=str, required=True)
     parser.add_argument('--dataset_name', type=str, required=True)
+    parser.add_argument('--runs', default=1, type=int)
 
     # Common Model Parameters
     parser.add_argument('--num_layers', default=3, type=int)
@@ -68,7 +68,7 @@ def test(model, data, split_idx, evaluator):
         'y_true': data.y[split_idx['test']],
         'y_pred': y_pred[split_idx['test']],
     })['acc']
-    
+
     del data, split_idx, out, y_pred
     torch.cuda.empty_cache()
 
@@ -78,21 +78,39 @@ if __name__ == '__main__':
     args = vars(get_parser().parse_args())
 
     data, dataset, split_idx, evaluator = get_data(args)
-    model = get_model(dataset, args)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=args['lr'], weight_decay=args['weight_decay'])
     loss_fn = F.nll_loss
 
-    best_valid_acc = 0
-    for epoch in range(1, args['epochs'] + 1):
-        loss = train(model, data, split_idx['train'], optimizer, loss_fn, args['dropedge'])
-        train_acc, valid_acc, test_acc = test(model, data, split_idx, evaluator)
+    best_train_accs, best_valid_accs, best_test_accs = [], [], []
+    for run in range(1, args['runs'] + 1):
+        print('Run ' + str(run))
+        print('=' * 20)
 
-        if valid_acc > best_valid_acc:
-            best_valid_acc = valid_acc
+        model = get_model(dataset, args)
+        optimizer = torch.optim.Adam(model.parameters(), lr=args['lr'], weight_decay=args['weight_decay'])
 
-        print(f'Epoch: {epoch:02d}, '
-              f'Loss: {loss:.4f}, '
-              f'Train: {100 * train_acc:.2f}%, '
-              f'Valid: {100 * valid_acc:.2f}% '
-              f'Test: {100 * test_acc:.2f}%')
+        best_train_acc, best_valid_acc, best_test_acc = 0, 0, 0
+        for epoch in range(1, args['epochs'] + 1):
+            loss = train(model, data, split_idx['train'], optimizer, loss_fn, args['dropedge'])
+            train_acc, valid_acc, test_acc = test(model, data, split_idx, evaluator)
+
+            if valid_acc > best_valid_acc:
+                best_train_acc, best_valid_acc, best_test_acc = train_acc, valid_acc, test_acc
+
+            print(f'Epoch: {epoch:02d}, '
+                  f'Loss: {loss:.4f}, '
+                  f'Train: {100 * train_acc:.2f}%, '
+                  f'Valid: {100 * valid_acc:.2f}% '
+                  f'Test: {100 * test_acc:.2f}%')
+
+        best_train_accs.append(best_train_acc)
+        best_valid_accs.append(best_valid_acc)
+        best_test_accs.append(best_test_acc)
+
+        del model
+        print()
+
+    print('Final Results')
+    print('=' * 20)
+    print(f'Best Train Acc: {100*np.mean(best_train_accs):0.2f}% ± {100*np.std(best_train_accs):0.2f}%')
+    print(f'Best Valid Acc: {100*np.mean(best_valid_accs):0.2f}% ± {100*np.std(best_valid_accs):0.2f}%')
+    print(f'Best Test Acc: {100*np.mean(best_test_accs):0.2f}% ± {100*np.std(best_test_accs):0.2f}%')
