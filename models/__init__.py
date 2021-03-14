980f437ca2fb040c37b8856ed751b27bda62dd8f
NYArtist <nyartist8@gmail.com>
2021-03-14 00:21:03 -0500

    More prints

--- models/__init__.py
+++ models/__init__.py
@@ -7,6 +7,8 @@ def get_model(dataset, args):
     elif args['model_type'] == 'incepgcn':
         model_type = IncepGCN
 
+
+    print("Data num features and classes: ", dataset.num_node_features, dataset.num_classes)
     model = model_type(dataset.num_node_features, dataset.num_classes, args)
     model = model.to('cuda' if torch.cuda.is_available() else 'cpu')
 
