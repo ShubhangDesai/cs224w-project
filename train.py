f44beea690e1035d93e045e1f0a274f89b17adcf
NYArtist <nyartist8@gmail.com>
2021-03-17 02:57:52 -0400

    Updated FLAG

--- train.py
+++ train.py
@@ -46,31 +46,13 @@ def train(model, data, train_idx, optimizer, loss_fn, dropedge_rate, apply_flag,
 
 
     # Add FLAG: unbiased perturbation
-    # out = None
+    out = None
     loss = None
-    if apply_flag: # FLAG training
+    if apply_flag: # Train with FLAG
         loss, out = flag_train(model, data, train_label, train_idx, loss_fn, dropedge_rate, flag_steps, flag_step_size)
-
-        # # Initialize adversarial perturbations
-        # perturb = torch.FloatTensor(data.x.shape).uniform_(-flag_step_size, flag_step_size) # Uniformation perturbations
-        # perturb = perturb.to('cuda' if torch.cuda.is_available() else 'cpu').requires_grad_()
-
-        # out = model(data.x + perturb, data.adj_t)[train_idx]
-        # loss = loss_fn(out, train_label) / flag_n_steps
-
-        # for _ in range(flag_n_steps): # Gradient ascent
-        #     loss.backward()
-        #     perturb.data = (perturb.detach() + flag_step_size*torch.sign(perturb.grad.detach())).data # Perturbation gradient ascent
-        #     perturb.grad[:] = 0.
-
-        #     out = model(data.x + perturb, data.adj_t)[train_idx]
-        #     loss = loss_fn(out, train_label) / flag_n_steps
-
+    
     else:
-        # Get predictions
         out = model(data.x, data.adj_t)[train_idx]
-
-        # Compute loss
         loss = loss_fn(out, train_label)
 
     loss.backward()
