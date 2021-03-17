91c10984938ba42ca0b9dd6ae4a4ac0edf8b9538
NYArtist <nyartist8@gmail.com>
2021-03-17 02:17:23 -0400

    added flag function

--- models/FLAG.py
+++ models/FLAG.py
@@ -10,14 +10,14 @@ def flag_train(model, data, train_label, train_idx, loss_fn, dropedge_rate, flag
 	perturb = perturb.to('cuda' if torch.cuda.is_available() else 'cpu').requires_grad_()
 
 	out = model(data.x + perturb, data.adj_t)[train_idx]
-	loss = loss_fn(out, train_label) / flag_n_steps
+	loss = loss_fn(out, train_label) / flag_steps
 
-	for _ in range(flag_n_steps): # Gradient ascent
+	for _ in range(flag_steps): # Gradient ascent
 		loss.backward()
 		perturb.data = (perturb.detach() + flag_step_size*torch.sign(perturb.grad.detach())).data # Perturbation gradient ascent
 		perturb.grad[:] = 0.
 
 		out = model(data.x + perturb, data.adj_t)[train_idx]
-		loss = loss_fn(out, train_label) / flag_n_steps
+		loss = loss_fn(out, train_label) / flag_steps
 
 	return loss, out
