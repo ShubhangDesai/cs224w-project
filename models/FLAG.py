a973c308a87c53dab77c87268ff29de8d7ca64a4
NYArtist <nyartist8@gmail.com>
2021-03-17 02:15:17 -0400

    added flag function

--- models/FLAG.py
+++ models/FLAG.py
@@ -4,19 +4,20 @@ import numpy as np
 
 def flag_train(model, data, train_idx, loss_fn, dropedge_rate, flag_steps, flag_step_size):
 	print("doing_flag")
-    # Initialize adversarial perturbations
-    perturb = torch.FloatTensor(data.x.shape).uniform_(-flag_step_size, flag_step_size) # Uniformation perturbations
-    perturb = perturb.to('cuda' if torch.cuda.is_available() else 'cpu').requires_grad_()
+	
+	# Initialize adversarial perturbations
+	perturb = torch.FloatTensor(data.x.shape).uniform_(-flag_step_size, flag_step_size) # Uniformation perturbations
+	perturb = perturb.to('cuda' if torch.cuda.is_available() else 'cpu').requires_grad_()
 
-    out = model(data.x + perturb, data.adj_t)[train_idx]
-    loss = loss_fn(out, train_label) / flag_n_steps
+	out = model(data.x + perturb, data.adj_t)[train_idx]
+	loss = loss_fn(out, train_label) / flag_n_steps
 
-    for _ in range(flag_n_steps): # Gradient ascent
-        loss.backward()
-        perturb.data = (perturb.detach() + flag_step_size*torch.sign(perturb.grad.detach())).data # Perturbation gradient ascent
-        perturb.grad[:] = 0.
+	for _ in range(flag_n_steps): # Gradient ascent
+		loss.backward()
+		perturb.data = (perturb.detach() + flag_step_size*torch.sign(perturb.grad.detach())).data # Perturbation gradient ascent
+		perturb.grad[:] = 0.
 
-        out = model(data.x + perturb, data.adj_t)[train_idx]
-        loss = loss_fn(out, train_label) / flag_n_steps
+		out = model(data.x + perturb, data.adj_t)[train_idx]
+		loss = loss_fn(out, train_label) / flag_n_steps
 
-    return loss, out
+	return loss, out
