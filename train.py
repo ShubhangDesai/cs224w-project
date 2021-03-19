ee26fdd2056db2f7aa7beb67beedb62e117928d8
Ubuntu <ubuntu@ip-172-31-6-247.us-west-2.compute.internal>
2021-03-19 06:32:59 +0000

    add flag

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from losses import *
 from models import *
 
 import numpy as np
-import argparse
+import argparse, functools
 
 def get_parser():
     parser = argparse.ArgumentParser()
@@ -34,20 +34,46 @@ def get_parser():
     parser.add_argument('--lr', default=0.01, type=float)
     parser.add_argument('--epochs', default=100, type=int)
     parser.add_argument('--weight_decay', default=0, type=float)
+    parser.add_argument('--num_flag_steps', default=0, type=int)
+    parser.add_argument('--flag_step_size', default=1e-3, type=float)
 
     return parser
 
-def train(model, data, split_idx, optimizer, loss_fn):
-    model.train()
+def flag_steps(model, data, split_idx, loss_fn, num_flag_steps, flag_step_size):
+    perturb = torch.FloatTensor(data.x.shape).uniform_(-flag_step_size, flag_step_size) # Uniformation perturbation
+    perturb = perturb.to('cuda' if torch.cuda.is_available() else 'cpu').requires_grad_()
 
+    out, hiddens = model(data.x + perturb, data.adj_t)
+    loss = loss_fn(out, data.y.squeeze(1), split_idx, hiddens) / num_flag_steps
+
+    for _ in range(num_flag_steps): # Gradient ascent
+        loss.backward()
+        perturb.data = (perturb.detach() + flag_step_size * torch.sign(perturb.grad.detach())).data # Perturbation gradient ascent
+        perturb.grad[:] = 0.
+
+        out, hiddens = model(data.x + perturb, data.adj_t)
+        loss = loss_fn(out, data.y.squeeze(1), split_idx, hiddens) / num_flag_steps
+
+    del out
+    torch.cuda.empty_cache()
+
+    return loss
+
+def train(model, data, split_idx, optimizer, loss_fn, num_flag_steps, flag_step_size):
+    model.train()
     optimizer.zero_grad()
-    out, hiddens = model(data.x, data.adj_t)
-    loss = loss_fn(out, data.y.squeeze(1), split_idx, hiddens)
+
+    if num_flag_steps == 0:
+        out, hiddens = model(data.x, data.adj_t)
+        loss = loss_fn(out, data.y.squeeze(1), split_idx, hiddens)
+    else:
+        loss = flag_steps(model, data, split_idx, loss_fn, num_flag_steps, flag_step_size)
 
     loss.backward()
     optimizer.step()
 
-    del data, split_idx, out, hiddens
+    del data, split_idx
+    if num_flag_steps == 0: del out, hiddens
     torch.cuda.empty_cache()
 
     return loss.item()
@@ -82,6 +108,7 @@ if __name__ == '__main__':
 
     data, dataset, split_idx, evaluator = get_data(args)
     loss_fn = get_loss(args)
+    train_fn = functools.partial(train, num_flag_steps=args['num_flag_steps'], flag_step_size=args['flag_step_size'])
 
     best_train_accs, best_valid_accs, best_test_accs = [], [], []
     for run in range(1, args['runs'] + 1):
@@ -93,7 +120,7 @@ if __name__ == '__main__':
 
         best_train_acc, best_valid_acc, best_test_acc = 0, 0, 0
         for epoch in range(1, args['epochs'] + 1):
-            loss = train(model, data, split_idx, optimizer, loss_fn)
+            loss = train_fn(model, data, split_idx, optimizer, loss_fn)
             train_acc, valid_acc, test_acc = test(model, data, split_idx, evaluator)
 
             if valid_acc > best_valid_acc:
